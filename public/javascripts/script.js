40bfb6a6f64d382de0b509656d7b0d572615bdbe
Gregory Olive <gregoryolive@outlook.com>
2022-04-14 14:45:18 +0900

    Build out main page structure

---
+++ public/javascripts/script.js
@@ -0,0 +1,7 @@
+const toggleMenu = () => {
+  const menu = document.querySelector('.header-links');
+  menu.classList = (menu.classList.contains('open')) ? 'header-links close' : 'header-links open';
+};
+
+document.querySelector('.menu-open').addEventListener('click', toggleMenu);
+document.querySelector('.menu-close').addEventListener('click', toggleMenu);
