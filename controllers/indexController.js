82f8a424e45225ef9e36bb46117af66a6bf2867d
Gregory Olive <gregoryolive@outlook.com>
2022-04-18 00:59:19 +0900

    Add control for portfolio detail and portfolio on index page!

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,9 +1,14 @@
 const passport = require('passport');
+const Portfolio = require('../models/portfolio');
 
 // Display home page if a user is loged in or redirect to log in.
 exports.home = (req, res, next) => {
-  const message = req.session.message;
-  res.render('index', { title: 'Home', user: req.user, message: message });
+  Portfolio.findOne({ 'owner': req.user._id })
+    .exec(function (err, portfolio) {
+      if (err) { return next(err); }
+      // Successful, so render.
+      res.render('index', { title: 'Home', user: req.user, portfolio: portfolio });
+    });
 };
 
 // Handle log in on GET.
--- controllers/portfolioController.js
+++ controllers/portfolioController.js
@@ -1,13 +1,28 @@
 const Portfolio = require('../models/portfolio');
 const { body, validationResult } = require('express-validator');
+const async = require('async');
 
 // Display detail page for a specific Portfolio.
-exports.portfolio_detail = (req, res) => {
-  res.send('NOT IMPLEMENTED: Portfolio detail: ' + req.params.id);
+exports.portfolio_detail = (req, res, next) => {
+  async.parallel({
+    portfolio: function(callback) {
+        Portfolio.findById(req.params.id)
+          .exec(callback);
+    },
+  }, function(err, results) {
+      if (err) { return next(err); }
+      if (results.portfolio == null) { // No results.
+          let err = new Error('Portfolio not found');
+          err.status = 404;
+          return next(err);
+      }
+      // Successful, so render
+      res.render('portfolio/portfolio_detail', { title: results.portfolio.name, user: req.user, portfolio: results.portfolio } );
+  });
 };
 
 // Display Portfolio create form on GET.
-exports.portfolio_create_get = (req, res) => {
+exports.portfolio_create_get = (req, res, next) => {
   res.render('portfolio/portfolio_form', { title: 'Create Portfolio', user: req.user });
 };
 
@@ -47,21 +62,21 @@ exports.portfolio_create_post = [
 ];
 
 // Display Portfolio delete form on GET.
-exports.portfolio_delete_get = (req, res) => {
+exports.portfolio_delete_get = (req, res, next) => {
   res.send('NOT IMPLEMENTED: Portfolio delete GET');
 };
 
 // Handle Portfolio delete on POST.
-exports.portfolio_delete_post = (req, res) => {
+exports.portfolio_delete_post = (req, res, next) => {
   res.send('NOT IMPLEMENTED: Portfolio delete POST');
 };
 
 // Display Portfolio update form on GET.
-exports.portfolio_update_get = (req, res) => {
+exports.portfolio_update_get = (req, res, next) => {
   res.send('NOT IMPLEMENTED: Portfolio update GET');
 };
 
 // Handle Portfolio update on POST.
-exports.portfolio_update_post = (req, res) => {
+exports.portfolio_update_post = (req, res, next) => {
   res.send('NOT IMPLEMENTED: Portfolio update POST');
 };
